a6c9548d1de96de04379b98ccd421ffad07fed16
Hans-techspot <hansade2005@gmail.com>
2025-10-04 10:28:48 +0100

    Update project files - 10/4/2025, 10:28:34 AM

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Phone, Mail, MapPin, Facebook, Twitter, Linkedin, Instagram } from 'luc
 
 const Footer = (): JSX.Element => {
   return (
-    <footer className="bg-green-800 text-white py-12 w-full">
+    <footer className="bg-green-800 text-white py-12 w-screen">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid md:grid-cols-4 gap-8 mb-8">
           {/* Company Info */}
